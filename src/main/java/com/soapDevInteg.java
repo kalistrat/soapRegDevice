3ac6eb90d3976e4de521ede65449c965955615a2
kalistrat <n7.semenov@gmail.com>
2018-03-07 00:56:47 +0300

    Изменил наименования методов
    Изменил наименования методов

--- src/main/java/com/soapDevInteg.java
+++ src/main/java/com/soapDevInteg.java
@@ -6,14 +6,21 @@ import javax.jws.WebService;
 @WebService(endpointInterface = "com.soapDevIntegrations")
 public class soapDevInteg implements soapDevIntegrations {
 
-    public String getDeviceInitialData(String text) {
-        System.out.println("Я изменил обработчик " + text);
-        return "Hello " + text;
+    public String linkUserDevice(String UID_CHAIN) {
+        System.out.println("UID_CHAIN: " + UID_CHAIN);
+        String resValue;
+        if (UID_CHAIN.equals("TEST_CHAIN")) {
+            resValue = "mqttLogin:qwerty1;mqttPassword:hfey67fe;mqttTopic:/SEN-002;";
+        } else {
+            resValue = "DEVICE_NOT_FOUND;";
+        }
+        return resValue;
     }
 
-    public String setDeviceInitialData(String text) {
-        System.out.println("Я установил обработчик " + text);
-        return "Hello " + text;
+    public String setUserDevice(String UID, String USER_DB_HOST) {
+        System.out.println("UID: " + UID);
+        System.out.println("USER_DB_HOST: " + USER_DB_HOST);
+        return "The device with the UID: " + UID + " was successfully added to the buffer";
     }
 }
 
--- src/main/java/com/soapDevIntegrations.java
+++ src/main/java/com/soapDevIntegrations.java
@@ -4,7 +4,7 @@ import javax.jws.WebService;
 
 @WebService
 public interface soapDevIntegrations {
-    String getDeviceInitialData(String text);
-    String setDeviceInitialData(String text);
+    String linkUserDevice(String UID_CHAIN);
+    String setUserDevice(String UID, String USER_DB_HOST);
 }
 

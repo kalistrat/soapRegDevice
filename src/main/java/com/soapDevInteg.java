3598dbd388cc50a247b8271a6cf5b4ada752905d
kalistrat <n7.semenov@gmail.com>
2018-03-11 19:44:33 +0300

    common web service initial
    common web service initial

--- src/main/java/com/soapDevInteg.java
+++ src/main/java/com/soapDevInteg.java
@@ -1,19 +1,48 @@
 
 package com;
 
+import javax.annotation.Resource;
 import javax.jws.WebService;
+import javax.xml.ws.WebServiceContext;
+import javax.xml.ws.handler.MessageContext;
+import java.util.List;
+import java.util.Map;
 
 @WebService(endpointInterface = "com.soapDevIntegrations")
 public class soapDevInteg implements soapDevIntegrations {
+    @Resource
+    WebServiceContext webServiceContext;
+
 
     public String linkUserDevice(String UID_CHAIN) {
         System.out.println("UID_CHAIN: " + UID_CHAIN);
         String resValue;
+        MessageContext messageContext = webServiceContext.getMessageContext();
+
+        Map<?,?> requestHeaders = (Map<?,?>) messageContext.get(MessageContext.HTTP_REQUEST_HEADERS);
+        List<?> usernameList = (List<?>) requestHeaders.get("username");
+        List<?> passwordList = (List<?>) requestHeaders.get("password");
+
+        String username = "";
+        String password = "";
+
+        if (usernameList != null) {
+            username = usernameList.get(0).toString();
+        }
+
+        if (passwordList != null) {
+            password = passwordList.get(0).toString();
+        }
+
+        System.out.println("username : " + username);
+        System.out.println("password : " + password);
+
         if (UID_CHAIN.equals("TEST_CHAIN")) {
             resValue = "mqttLogin:qwerty1;mqttPassword:hfey67fe;mqttTopic:/SEN-002;";
         } else {
             resValue = "DEVICE_NOT_FOUND;";
         }
+
         return resValue;
     }
 
